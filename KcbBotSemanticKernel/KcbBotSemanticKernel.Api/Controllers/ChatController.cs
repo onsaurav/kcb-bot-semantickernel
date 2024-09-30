fd297c1b8dfc7abf86ffa1588db17fde89c1ded6
kartik.biswas <kartik.biswas@itmagnet.com.au>
2024-09-30 17:41:54 +0600

    API added and hosted in Azure

---
+++ KcbBotSemanticKernel/KcbBotSemanticKernel.Api/Controllers/ChatController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KcbBotSemanticKernel.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
